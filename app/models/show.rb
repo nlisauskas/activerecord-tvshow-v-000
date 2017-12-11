d15db8233fee86eab2f8fe2eca8dcba711425163
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-11 17:10:46 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
   end
 
   def lowest_rating
-    self.where("rating = ?", self.minimum(:rating))
+    Show.where("rating = ?", Show.minimum(:rating))
   end
 
   def least_popular_show
